5a426ba336ae7e4a41466e76834966f8480ad348
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-10-06 05:26:57 +0600

    init commit

--- app.js
+++ app.js
@@ -10,20 +10,27 @@ var crypto         = require('crypto');
 var express        = require('express');
 var http           = require('http');
 var path           = require('path');
-var engine         = require('ejs-locals');
+var ejsEngine      = require('ejs-locals');
 var cookieParser   = require('cookie-parser');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var logger         = require('morgan');
 var errorHandler   = require('errorhandler');
 var optional       = require('optional');
+var marked         = require('marked');
+var fileUpload     = require('express-fileupload');
+var dust           = require('dustjs-linkedin');
+var dustHelpers   = require('dustjs-helpers');
+var cons           = require('consolidate');
 
 var app    = express();
 var routes = require('./routes');
 
 // all environments
 app.set('port', process.env.PORT || 3001);
-app.engine('ejs', engine);
+app.engine('ejs', ejsEngine);
+app.engine('dust', cons.dust);
+cons.dust.helpers = dustHelpers;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
@@ -31,17 +38,25 @@ app.use(methodOverride());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(fileUpload());
 
 // Routes
 app.use(routes.current_user);
 app.get('/', routes.index);
+app.get('/admin', routes.admin);
+app.post('/admin', routes.admin);
 app.post('/create', routes.create);
 app.get('/destroy/:id', routes.destroy);
 app.get('/edit/:id', routes.edit);
 app.post('/update/:id', routes.update);
-
+app.post('/import', routes.import);
+app.get('/about_new', routes.about_new);
 // Static
 app.use(st({path: './public', url: '/public'}));
+
+// Add the option to output (sanitized!) markdown
+marked.setOptions({ sanitize: true });
+app.locals.marked = marked;
 
 // development only
 if (app.get('env') == 'development') {
